89a9405b494b1d67d8633c725b2f5351f5a9f845
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-05 22:28:16 +0530

    Update Count Characters.cpp

--- Basic Data Types/Count Characters.cpp
+++ Basic Data Types/Count Characters.cpp
@@ -1,3 +1,18 @@
+/* Count Characters (100 Marks)
+You will be given a string S, consisting of lowercase English letters and a character C.
+Your task is to find the number of occurrences of that character in the string. 
+
+Input Format
+First line will contain a string S.
+Next line will contain a character C.
+
+Constraints
+1 <= S <= 10^3
+
+Output Format
+Output the number of occurrences of character C in the string S. 
+*/
+
 #include <iostream>
 #include<cstring>
 using namespace std;
