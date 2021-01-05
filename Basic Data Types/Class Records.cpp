0466ce7bc794ab3a582002c0f3923ca7f3cdc395
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-05 22:27:24 +0530

    Update Class Records.cpp

--- Basic Data Types/Class Records.cpp
+++ Basic Data Types/Class Records.cpp
@@ -1,3 +1,20 @@
+/* Class Records (100 Marks)
+You are given a dataset of N students belonging to the same class.
+The data contains name of the student followed by marks they scored in five subjects which are Physics, Chemsistry, Maths, English, Hindi.
+Your task is find the average marks of the class for each individual subject.
+
+Input Format
+First line will contain an Integer N, denoting  the number of students in the class.
+Each of the Next N lines will contain a string S denoting the student name, followed by five integers m1, m2, m3, m4, m5 denoting the marks scored in each subject.
+
+Constraints
+1 <= N <= 10^3
+1 <= S <= 10^2
+0 <= m1, m2, m3, m4, m5 <= 100
+
+Output Format
+You have to print average marks upto two decimal places for each subject followed by a space.*/
+
 #include <iostream>
 #include<cstring>
 #include<bits/stdc++.h>
